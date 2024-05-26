e4ac08a4fb176e92132280c3666346dc0d407939
Barclay-McClay <chrisapeitos@hotmail.com>
2024-05-26 14:21:47 +0000

    Daily update

--- uploader.sh
+++ uploader.sh
@@ -1,8 +1,12 @@
 # This script is run once every-day on a server to commit changes to a git repository
-# The script is run using a cron job
 
-# Change to the directory where the files are stored
-cd ~/path/to/directory
+source .env
+	
+
+git config --global user.email $GITHUB_EMAIL
+git config --global user.name $GITHUB_NAME
+
+git remote set-url origin https://$GITHUB_TOKEN@github.com/$GITHUB_NAME/Daily-Journaler.git/
 
 # Add all files to the git repository
 git add .
@@ -11,6 +15,6 @@ git add .
 git commit -m "Daily update"
 
 # Push the changes to the remote repository
-git push origin master
+git push https://$GITHUB_NAME:$GITHUB_TOKEN@github.com/$GITHUB_NAME/Daily-Journaler.git/
 
 # Exit the script
